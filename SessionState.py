ecba488b584c38eada6e0f832df35dce881370d1
Zhenia-Magic <evgeniia@minerva.kgi.edu>
2022-05-23 10:27:52 +0300

    fixed bug with new versions of streamlit

--- SessionState.py
+++ SessionState.py
@@ -16,8 +16,8 @@ result:
 'Mary'
 """
 try:
-    import streamlit.script_run_context as ReportThread
-    from streamlit.server import Server
+    from streamlit.scriptrunner.script_run_context import get_script_run_ctx
+    from streamlit.server.server import Server
 except Exception:
     # Streamlit >= 0.65.0
     import streamlit.script_run_context as ReportThread
